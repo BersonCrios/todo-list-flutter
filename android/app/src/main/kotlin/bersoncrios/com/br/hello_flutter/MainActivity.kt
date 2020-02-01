7199118700618163bcf1b22609c85f3cd8c0a4eb
bersondev <bersondev@gmail.com>
2020-02-01 16:59:13 -0300

    Adicionando item, listando item e excluindo

---
+++ android/app/src/main/kotlin/bersoncrios/com/br/hello_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package bersoncrios.com.br.hello_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
